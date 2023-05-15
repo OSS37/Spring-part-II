99b670f6e9a69b28c4cbabc5e22d6554033b0046
OSS37 <suhanova375@gmail.com>
2023-05-15 17:17:16 +0300

    initial commit

---
+++ src/main/java/va/suhano/SpringpartII/SpringPartIiApplication.java
@@ -0,0 +1,13 @@
+package va.suhano.SpringpartII;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPartIiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPartIiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/va/suhano/SpringpartII/SpringPartIiApplicationTests.java
@@ -0,0 +1,13 @@
+package va.suhano.SpringpartII;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPartIiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
